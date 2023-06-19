891dcb7cade64d295aefcd240bc8863722983ae7
ayushdahl10 <46618058+ayushdahl10@users.noreply.github.com>
2023-06-19 20:53:17 +0545

    added player and control

--- src/pixel.js
+++ src/pixel.js
@@ -1,24 +1,61 @@
-var pixiapp = new PIXI.Application({   
-    background: "#1099bb",
-    resizeTo: window });
-
+var pixiapp = new PIXI.Application({ background: "#1099bb", resizeTo: window });
 document.body.appendChild(pixiapp.view);
 
 const main={
     config:{
-        background:'',
+        background:null,
+        player:null,
+        player_pos:0,
         scale:1
     },
+    //load game content 
     load_content:function()
     {
         // Load the background image
         this.config.background=PIXI.Sprite.from('assets/background/background.jpg')
+        this.config.player=PIXI.Sprite.from('assets/player/idle.png')
+        this.config.player.scale.x=2;
+        this.config.player.scale.y=2;
         pixiapp.renderer.resize(window.innerWidth, window.innerHeight);
     },
+    //draw sprite in screen
     draw:function()
     {
         pixiapp.stage.addChild(this.config.background)
+        pixiapp.stage.addChild(this.config.player)
     },
+    //call your game logic here
+    update:function()
+    {   
+
+        this.resize();
+        window.addEventListener('resize', this.resize.bind(this));
+        requestAnimationFrame(this.update_player_position())
+    },
+    update_player_position:function()
+    {
+        document.addEventListener('keydown', (event) => {
+            const speed = 10; // Adjust the speed as desired
+          
+            // Move the player based on the key pressed
+            switch (event.code) {
+              case 'ArrowUp':
+                this.config.player.y -= speed;
+                break;
+              case 'ArrowDown':
+                this.config.player.y += speed;
+                break;
+              case 'ArrowLeft':
+                this.config.player.x -= speed;
+                break;
+              case 'ArrowRight':
+                this.config.player.x += speed;
+                break;
+            }
+          });
+          requestAnimationFrame(this.update_player_position());
+    },
+    //resize the pixel images 
     resize:function()
     {   
         const scale = Math.min(window.innerWidth / pixiapp.screen.width, window.innerHeight / pixiapp.screen.height);
@@ -35,8 +72,8 @@ const main={
     {
         this.load_content();
         this.draw();
-        this.resize();
-        window.addEventListener('resize', this.resize.bind(this));
+        this.update();
+
     }
 }
 main.run();
